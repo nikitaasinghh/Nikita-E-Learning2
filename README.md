853d32447ba53dc13774be9b57cc64d10cc1580c
Nikita Singh <nikitasingh.1022@gmail.com>
2023-06-23 19:20:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Nikita-E-Learning
+# E-commerce Store Using React & Context API 
+
+### https://your-online-store.netlify.app/
---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="container">
+      <header>
+        <div className="header-text">
+          <h1>All Courses Available Here !</h1>
+          <p>Enjoy Online Learning With Us</p>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Banner;
